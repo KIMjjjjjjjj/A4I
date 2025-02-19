9021c761a7ba29bcf0fd3a45f8b9f63a5173a6d3
Min Hyuk <ccmintt@gmail.com>
2025-02-19 14:43:30 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'signup.dart';
+import 'findPassword.dart';
+import 'signup.dart';
+import 'findID.dart';
+import 'loginPage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/findID': (context) => FindIDPage(),
+        '/findPassword': (context) => FindPasswordPage(),
+        '/signUp': (context) => SignUpPage(),
+      },
+    );
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Asia/Seoul'));
+
+  runApp(MyApp());
+}
