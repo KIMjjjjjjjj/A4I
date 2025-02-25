09bdf7f8ffd9d0154cd3d78faf3d81e88350f132
NaJunSun <nayoskj@hansung.ac.kr>
2025-02-26 00:39:31 +0900

    Seconds feature main UI & DayLine Dev seconds commit - JS

--- lib/UI/DayLine/day_line_UI.dart
+++ lib/UI/DayLine/day_line_UI.dart
@@ -1,14 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'group_chat_UI.dart';
 
-class DayLineScreen extends StatelessWidget {
-  final String visitorCount = "25"; // 방문자 수
+class DayLineScreen extends StatefulWidget {
+  @override
+  _DayLineScreenState createState() => _DayLineScreenState();
+}
+
+class _DayLineScreenState extends State<DayLineScreen> {
+  int visitorCount = 0; // 방문자 수
+  String dayTopic = "";
+  final User? user = FirebaseAuth.instance.currentUser;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String visitorsDocumentId = "I4mc3vHFE0dkWVC9dfmr";
+  final String topicDocumentId = "tZsVTdoaMu3pDYagWUR1";
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchVisitorCount();
+    _fetchDayTopic();
+  }
+
+  // 방문자 수 가져오기
+  Future<void> _fetchVisitorCount() async {
+    try {
+      DocumentSnapshot doc =
+      await _firestore.collection('day_line').doc(visitorsDocumentId).get();
+      if (doc.exists) {
+        setState(() {
+          visitorCount = doc['visitors'] ?? 0;
+        });
+      }
+    } catch (e) {
+      print("방문자 수 가져오기 오류: $e");
+    }
+  }
+
+  Future<void> _fetchDayTopic() async {
+    try {
+      DocumentSnapshot doc =
+      await _firestore.collection('day_line').doc(topicDocumentId).get();
+      if (doc.exists) {
+        List<String> topics = List<String>.from(doc['day_topic']);
+        int today = DateTime.now().day;
+        setState(() {
+          dayTopic = topics[today - 1];
+        });
+      }
+    } catch (e) {
+      print("오늘의 주제 가져오기 오류: $e");
+    }
+  }
+
+  // 방문자 수 증가시키기
+  Future<void> _incrementVisitorCount() async {
+    try {
+      DocumentReference docRef =
+      _firestore.collection('day_line').doc(visitorsDocumentId);
+      await _firestore.runTransaction((transaction) async {
+        DocumentSnapshot snapshot = await transaction.get(docRef);
+        if (!snapshot.exists) {
+          throw Exception("문서가 존재하지 않습니다.");
+        }
+        int newCount = (snapshot['visitors'] ?? 0) + 1;
+        transaction.update(docRef, {'visitors': newCount});
+        setState(() {
+          visitorCount = newCount;
+        });
+      });
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const GroupChatScreen()),
+      );
+    } catch (e) {
+      print("방문자 수 업데이트 오류: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEAEBF0),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(27), // 앱바 높이를 40으로 설정
+        preferredSize: Size.fromHeight(27),
         child: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
@@ -42,7 +118,7 @@ class DayLineScreen extends StatelessWidget {
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    visitorCount,
+                    "$visitorCount",
                     style: TextStyle(
                       fontSize: 40,
                       height: 21 / 40,
@@ -53,27 +129,28 @@ class DayLineScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 25),
 
-                // speech_bubble 안에 텍스트 넣기
                 Stack(
                   alignment: Alignment.center,
                   children: [
-                    Image.asset(
-                      'assets/images/DayLine/speech_bubble.png',
-                    ),
+                    Image.asset('assets/images/DayLine/speech_bubble.png'),
                     Positioned(
-                      child: Text(
-                        "오늘의 감사한 일을 적어주세요",
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                        textAlign: TextAlign.center,
+                      child: SizedBox(
+                        width: 200, // 말풍선 내부 텍스트가 들어갈 최대 너비 설정
+                        child: Text(
+                          dayTopic,
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                          textAlign: TextAlign.center,
+                          softWrap: true, // 자동 줄바꿈 허용
+                          overflow: TextOverflow.ellipsis, // 길면 ... 표시
+                          maxLines: 2, // 최대 2줄까지만 표시
+                        ),
                       ),
                     ),
                   ],
                 ),
 
                 SizedBox(height: 0),
-                Image.asset(
-                  'assets/images/DayLine/letter_HARU.png',
-                ),
+                Image.asset('assets/images/DayLine/letter_HARU.png'),
                 SizedBox(height: 24),
 
                 ElevatedButton(
@@ -84,7 +161,7 @@ class DayLineScreen extends StatelessWidget {
                     ),
                     fixedSize: Size(148, 57),
                   ),
-                  onPressed: () {},
+                  onPressed: _incrementVisitorCount, // 버튼 클릭 시 방문자 수 증가
                   child: Text(
                     "입장하기",
                     style: TextStyle(fontSize: 16, color: Colors.black),
---
+++ lib/UI/DayLine/group_chat_UI.dart
@@ -0,0 +1,339 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart'; // 날짜 및 시간 포맷을 위해 추가
+
+class GroupChatScreen extends StatefulWidget {
+  const GroupChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _GroupChatScreenState createState() => _GroupChatScreenState();
+}
+
+class _GroupChatScreenState extends State<GroupChatScreen> {
+  final String defaultProfileImageUrl = 'assets/images/DayLine/default_profile.png';
+  String dayTopic = "";
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String currentUserUid;
+  final String topicDocumentId = "tZsVTdoaMu3pDYagWUR1";
+  final String textDocumentId = "oiXm2LUYnE4U20OI3VMx";
+  final TextEditingController _textController = TextEditingController();
+
+  // 텍스트 색상 리스트
+  final List<String> colorList = [
+    "FFF7EF", "FFEFEF", "F7FFEF", "EFF6FF",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentUserUid = _auth.currentUser?.uid ?? '';
+    _fetchDayTopic();
+  }
+
+  Future<void> _fetchDayTopic() async {
+    try {
+      DocumentSnapshot doc = await _firestore.collection('day_line').doc(topicDocumentId).get();
+      if (doc.exists) {
+        List<String> topics = List<String>.from(doc['day_topic']);
+        int today = DateTime.now().day;
+        setState(() {
+          dayTopic = topics[today - 1];
+        });
+      }
+    } catch (e) {
+      print("오늘의 주제 가져오기 오류: $e");
+    }
+  }
+
+  Future<List<Widget>> _fetchMessages() async {
+    List<Widget> messageWidgets = [];
+    try {
+      print("메시지 가져오기 시작...");  // 디버깅: 시작 출력
+
+      QuerySnapshot snapshot = await _firestore.collection('day_line')
+          .doc(textDocumentId)
+          .collection('messages')
+          .get();
+
+      print("메시지 가져오기 완료, 총 ${snapshot.docs.length}개의 메시지");  // 디버깅: 가져온 메시지 개수 출력
+
+      for (var doc in snapshot.docs) {
+        try {
+          String uid = doc['uid'];
+          String lineText = doc['line_text'];
+          String lineColor = doc['line_color'];
+          Timestamp timestamp = doc['text_time'];
+
+          print("메시지 데이터: uid=$uid, lineText=$lineText, lineColor=$lineColor, timestamp=$timestamp"); // 디버깅: 각 메시지 데이터 출력
+
+          // 사용자 정보 가져오기
+          DocumentSnapshot userDoc = await _firestore.collection('register').doc(uid).get();
+          String nickname = userDoc['nickname'];
+          String? profileImageUrl = userDoc['profileImageUrl'];
+
+          // profileImageUrl이 null일 경우 기본 이미지 URL 사용
+          String imageUrlToUse = profileImageUrl ?? defaultProfileImageUrl;
+
+          print("사용자 정보: nickname=$nickname, profileImageUrl=$profileImageUrl"); // 디버깅: 사용자 정보 출력
+
+          // 현재 사용자 채팅 패널과 다른 사용자 채팅 패널을 구분
+          if (uid == currentUserUid) {
+            messageWidgets.add(currentUserChatPanel(nickname, imageUrlToUse, lineColor, lineText, timestamp));
+          } else {
+            messageWidgets.add(otherUserChatPanel(nickname, imageUrlToUse, lineColor, lineText, timestamp));
+          }
+        } catch (e) {
+          print("메시지 처리 중 오류: $e");  // 디버깅: 메시지 처리 오류 출력
+        }
+      }
+    } catch (e) {
+      print("메시지 가져오기 오류: $e");  // 디버깅: 메시지 가져오기 오류 출력
+    }
+    return messageWidgets;
+  }
+
+
+
+  // 메시지 전송 시간 계산 (분, 시간, 일 단위로 출력)
+  String _getTimeAgo(Timestamp timestamp) {
+    DateTime messageTime = timestamp.toDate();
+    Duration diff = DateTime.now().difference(messageTime);
+    if (diff.inDays > 0) {
+      return '${diff.inDays}일 전';
+    } else if (diff.inHours > 0) {
+      return '${diff.inHours}시간 전';
+    } else {
+      return '${diff.inMinutes}분 전';
+    }
+  }
+
+// 현재 사용자 채팅 패널
+  Widget currentUserChatPanel(String nickname, String profileImageUrl, String lineColor, String lineText, Timestamp timestamp) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 23,
+            backgroundImage: profileImageUrl == defaultProfileImageUrl
+                ? AssetImage(defaultProfileImageUrl) as ImageProvider
+                : NetworkImage(profileImageUrl),
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                nickname,
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              SizedBox(height: 5),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.6, // ✅ 최대 너비 제한
+                ),
+                decoration: BoxDecoration(
+                  color: Color(int.parse("0xFF$lineColor")),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                child: Text(
+                  lineText,
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                _getTimeAgo(timestamp),
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  // 다른 사용자 채팅 패널
+  Widget otherUserChatPanel(String nickname, String profileImageUrl, String lineColor, String lineText, Timestamp timestamp) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                nickname,
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              SizedBox(height: 5),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(int.parse("0xFF$lineColor")),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                child: Stack(
+                  children: [
+                    Text(
+                      lineText,
+                      style: TextStyle(color: Colors.black),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: null, // 자동 줄바꿈
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                _getTimeAgo(timestamp),
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+            ],
+          ),
+          SizedBox(width: 10),
+          CircleAvatar(
+            radius: 23,
+            backgroundImage: profileImageUrl == defaultProfileImageUrl
+                ? AssetImage(defaultProfileImageUrl) as ImageProvider
+                : NetworkImage(profileImageUrl),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  void _sendMessage() async {
+    String message = _textController.text;
+    if (message.trim().isNotEmpty) {
+      String uid = FirebaseAuth.instance.currentUser?.uid ?? '';
+      String color = (colorList..shuffle()).first;  // 색상 랜덤으로 선택
+      Timestamp timestamp = Timestamp.now();
+
+      try {
+        await _firestore.collection('day_line').doc('oiXm2LUYnE4U20OI3VMx').collection('messages').add({
+          'line_color': color,
+          'line_text': message,
+          'text_time': timestamp,
+          'uid': uid,
+        });
+
+        print("메시지 전송 완료!");
+        _textController.clear();
+      } catch (e) {
+        print("메시지 전송 오류: $e");
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE6FFFD),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          flexibleSpace: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                bottom: 0,
+                child: Image.asset(
+                  'assets/images/DayLine/topic_cloud.png',
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                ),
+              ),
+              Positioned(
+                bottom: 30,
+                child: Text(
+                  dayTopic,
+                  style: TextStyle(fontSize: 18, height: 21 / 20, color: Color(0xFF4F4949)),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            child: FutureBuilder<List<Widget>>(
+              future: _fetchMessages(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text("오류 발생: ${snapshot.error}"));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text("메시지가 없습니다."));
+                } else {
+                  return ListView(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    children: snapshot.data!,
+                  );
+                }
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset(
+              'assets/images/DayLine/background_image.png',
+            ),
+          ),
+          Container(
+            color: Color(0xFFD9AEAE),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Center(
+              child: FractionallySizedBox(
+                widthFactor: 1,
+                child: Container(
+                  height: 43,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFE6BCA9),
+                    borderRadius: BorderRadius.circular(21.5),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.add, color: Colors.black),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: TextField(
+                          controller: _textController,
+                          decoration: InputDecoration(
+                            hintText: "자유롭게 한 줄을 적어보세요.",
+                            border: InputBorder.none,
+                            isDense: true,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: _textController.text.trim().isEmpty ? null : _sendMessage,
+                        child: Icon(
+                          Icons.send,
+                          color: _textController.text.trim().isEmpty ? Colors.grey : Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
