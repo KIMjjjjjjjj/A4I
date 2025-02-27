5f44cf7e86b613ebac0dafbc9d05cfdeb9bb406b
GMYUN <rookie0717@naver.com>
2025-02-27 18:45:15 +0900

    Add files via upload

---
+++ lib/UI/diary/view_diary.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ViewDiary extends StatefulWidget {
+  final String userId;
+  final String year;
+  final String month;
+  final String day;
+
+  const ViewDiary({
+    Key? key,
+    required this.userId,
+    required this.year,
+    required this.month,
+    required this.day,
+  }) : super(key: key);
+
+  @override
+  _ViewDiaryState createState() => _ViewDiaryState();
+}
+
+class _ViewDiaryState extends State<ViewDiary> {
+  String facePath = 'assets/default.png';
+  String date = '';
+  String title = '';
+  String content = '';
+  String imgPath = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDiaryData();
+  }
+
+  Future<void> _fetchDiaryData() async {
+    try {
+      DocumentSnapshot snapshot = await FirebaseFirestore.instance
+          .collection('diary')
+          .doc(widget.userId)
+          .collection('diary')
+          .doc(widget.year)
+          .collection(widget.month)
+          .doc(widget.day)
+          .get();
+
+      if (snapshot.exists) {
+        var data = snapshot.data() as Map<String, dynamic>;
+        setState(() {
+          facePath = data['face'] ?? '';
+          imgPath = data['imagePath'] ?? '';
+          title = data['title'] ?? '';
+          content = data['content'] ?? '';
+        });
+      }
+    } catch (e) {
+      print('오류');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE3F3FF),
+      appBar: AppBar(
+        title: Text(
+          widget.year,
+          style: TextStyle(
+            fontFamily: 'SingleDay',
+            color: Color(0xFF3F5C7E),
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xFFE6F0FA),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              height: 750,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        facePath,
+                        width: 40,
+                        height: 40,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(Icons.image_not_supported);
+                        },
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        '${widget.month}월 ${widget.day}일',
+                        style: TextStyle(
+                          fontFamily: 'SingleDay',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  if (imgPath.isNotEmpty)
+                    Container(
+                      height: 300,
+                      width: double.infinity,
+                      color: Colors.grey[200],
+                      child: imgPath.startsWith('http')
+                          ? Image.network(
+                        imgPath,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(Icons.broken_image, size: 50, color: Colors.grey);
+                        },
+                      )
+                          : Image.asset(
+                        imgPath,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(Icons.broken_image, size: 50, color: Colors.grey);
+                        },
+                      ),
+                    ),
+
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    content,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/UI/diary/write_diary.dart
@@ -0,0 +1,324 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class DiaryEntryPage extends StatefulWidget {
+  final String year;
+  final String month;
+  final String day;
+
+  const DiaryEntryPage({Key? key, required this.year, required this.month, required this.day,}) : super(key: key);
+
+  @override
+  _DiaryEntryPageState createState() => _DiaryEntryPageState();
+}
+
+class _DiaryEntryPageState extends State<DiaryEntryPage> {
+  String selectedFace = '';
+  File? _selectedImage;
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  final List<String> imagePaths = [
+    'assets/face/face1.png',
+    'assets/face/face2.png',
+    'assets/face/face3.png',
+    'assets/face/face4.png',
+    'assets/face/face5.png',
+    'assets/face/face6.png',
+    'assets/face/face7.png',
+    'assets/face/face8.png',
+    'assets/face/face9.png',
+    'assets/face/face10.png',
+    'assets/face/face11.png',
+    'assets/face/face12.png',
+    'assets/face/face13.png',
+    'assets/face/face14.png',
+    'assets/face/face15.png',
+    'assets/face/face16.png',
+    'assets/face/face17.png',
+    'assets/face/face18.png',
+    'assets/face/face19.png',
+    'assets/face/face20.png',
+    'assets/face/face21.png',
+    'assets/face/face22.png',
+    'assets/face/face23.png',
+    'assets/face/face24.png',
+    'assets/face/face25.png',
+    'assets/face/face26.png',
+    'assets/face/face27.png',
+    'assets/face/face28.png',
+    'assets/face/face29.png',
+    'assets/face/face30.png',
+
+
+  ];
+
+  Future<void> _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      final croppedImage = await ImageCropper().cropImage(
+        sourcePath: pickedImage.path,
+        aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      );
+      if (croppedImage != null) {
+        setState((){
+          _selectedImage = File(croppedImage.path);
+        });
+      }
+    }
+  }
+
+  Future<String?> uploadImage(File imageFile) async {
+    try {
+      String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+
+      Reference storageRef = FirebaseStorage.instance
+          .ref()
+          .child('diary_images/$fileName.jpg');
+
+      UploadTask uploadTask = storageRef.putFile(imageFile);
+
+      TaskSnapshot snapshot = await uploadTask;
+      String downloadUrl = await snapshot.ref.getDownloadURL();
+
+      return downloadUrl;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<void> SaveDiary() async {
+    try {
+      String uid = FirebaseAuth.instance.currentUser?.uid ?? '';
+
+      String year = widget.year;
+      String month = widget.month;
+      String day = widget.day;
+      String Date = '$year-$month-$day';
+
+      String? imageUrl;
+      if (_selectedImage != null) {
+        imageUrl = await uploadImage(_selectedImage!);
+      }
+
+      await FirebaseFirestore.instance
+          .collection('diary')
+          .doc(uid)
+          .collection('diary')
+          .doc(year)
+          .collection(month)
+          .doc(day)
+          .set({
+        'title': _titleController.text,
+        'content': _contentController.text,
+        'date': Date,
+        'face': selectedFace,
+        'imagePath': imageUrl ?? '',
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('저장되었습니다')),
+      );
+
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('오류')),
+      );
+    }
+  }
+
+  void _showEmojiPicker() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return DraggableScrollableSheet(
+          expand: false,
+          initialChildSize: 0.5,
+          minChildSize: 0.3,
+          maxChildSize: 0.8,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return GridView.builder(
+              controller: scrollController,
+              padding: EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 6,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: imagePaths.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedFace = imagePaths[index];
+                    });
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        imagePaths[index],
+                        width: 30,
+                        height: 30,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE3F3FF),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40),
+            Container(
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              height: 800,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: _showEmojiPicker,
+                            child: Container(
+                              child: Center(
+                                child: selectedFace.isNotEmpty
+                                    ? Image.asset(
+                                  selectedFace,
+                                  width: 30,
+                                  height: 30,
+                                  fit: BoxFit.cover,
+                                )
+                                    : Icon(
+                                  Icons.add_circle,
+                                  color: Colors.grey,
+                                  size: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Text('${widget.month}월 ${widget.day}일', style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Container(
+                    height: 300,
+                    color: Colors.grey[200],
+                    child: GestureDetector(
+                      onTap: _pickImage,
+                      child: Center(
+                        child: _selectedImage != null
+                            ? Image.file(
+                          _selectedImage!,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 300,
+                        )
+                            : Icon(Icons.image, size: 50, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      labelText: '제목',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  SizedBox(height: 1),
+                  TextField(
+                    controller: _contentController,
+                    decoration: InputDecoration(
+                      labelText: '일기를 작성해보세요',
+                      labelStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Color(0xFF71ABE8), width: 1.5),
+                          ),
+                          child: Text('취소', style: TextStyle(color: Colors.black54)),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      SizedBox(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: SaveDiary,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[200],
+                          ),
+                          child: Text('저장'),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
